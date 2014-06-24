dead2920a61f5528040a3b43774952b04f752b7d
Roland Gropmair <roland@mapps.ie>
2014-06-24 23:35:55 +0100

    added UI handling

--- Yolandia/ViewController.swift
+++ Yolandia/ViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 import CloudKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
-                            
+class ViewController: UIViewController, UITextFieldDelegate, UIAlertViewDelegate {
+    
     @IBOutlet var userNameTextField: UITextField
     
     override func viewDidLoad() {
@@ -18,16 +18,52 @@ class ViewController: UIViewController, UITextFieldDelegate {
         self.userNameTextField.delegate = self
         
     }
-
+    
     func textFieldShouldReturn(textField: UITextField!) -> Bool {
         println("textfield.text=\(textField.text)")
         var user: CKRecord
         var error: NSError
-        User.saveNewUser(textField.text, completionHandler: {(user, error) in
-            println("user=\(user)")
+        let userName = textField.text
+        
+        User.checkIfUserExists(userName, completionHandler: {(doesUserExist, error) in
+            if (error) {
+                self.showAlert("Error Checking User Name", message: "Please try again later")
+                
+            } else if (doesUserExist == true) {
+                self.showAlert("User already exists", message: "Please try another user name")
+                
+            } else {
+                
+                User.saveNewUser(userName, completionHandler: { (savedRecord, error) in
+                    
+                    if (savedRecord) {
+                        self.showAlert("Saved Your User Name", message: "Congrats - you claimed your user name")
+                        
+                    } else {
+                        self.showAlert("Somthing Went Wrong...", message: "Oops - could not claim that user name.\nPlease try again later")
+                        
+                    }
+                    })
+                
+            }
             })
+        
         return true
     }
-
+    
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        let defaultAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { action in
+            alert.dismissViewControllerAnimated(true, completion: nil)
+            })
+        alert.addAction(defaultAction)
+        
+        NSOperationQueue.mainQueue().addOperationWithBlock {
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
 }
 
